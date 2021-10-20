a0e48d20cf9eab7f06d6eac9df4704b63c672a9e
erika desu <erikadesu@gmx.com>
2021-10-20 17:34:38 -0400

    skip authorization if devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit
 
-  after_action :verify_authorized, except: :index
+  after_action :verify_authorized, except: :index, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index
 
 end
